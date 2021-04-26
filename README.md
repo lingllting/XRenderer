2821674c13984f69db62718d199c77e9aeddc65e
Liuyang <lingllting@vip.qq.com>
2021-04-26 23:17:50 +0800

    Add GLEW to the project.
    GLEW is a specification that allows you to access api pointers of modern OpenGL provided by the graphic card.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # XRenderer
  This is a general project for learing OpenGL and making a software rasterizer from scratch.
+
+ Libraries:
+ [GLFW](https://www.glfw.org/download.html)
+ [GLEW](http://glew.sourceforge.net/)
--- XRenderer/src/Application.cpp
+++ XRenderer/src/Application.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -18,6 +21,14 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		std::cout << "Error!!" << std::endl;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
